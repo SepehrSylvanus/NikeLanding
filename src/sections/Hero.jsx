ebbe9912d80becd4668a795eced6a4621227d923
unknown <mr.forte.world@gmail.com>
2024-02-27 14:09:39 +0330

    navbar

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,10 @@
-
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section
+      id="home"
+      className="w-full border-2 border-red-500 p-2 flex xl:flex-row flex-col justify-center min-h-screen"
+    ></section>
+  );
+};
 
-export default Hero
+export default Hero;
